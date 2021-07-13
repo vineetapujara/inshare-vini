ec05c63285101d22e805edc48b1928599449ec4c
vineetapujara <viniaswani70@gmail.com>
2021-07-13 10:55:20 +0530

    my

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.set("view engine", "ejs");
 // Routes
 app.use("/api/files", require("./routes/files"));
 app.use("/files", require("./routes/show"));
-app.use("/files/download", require("./routes/download"));
+app.use("files/download", require("./routes/download"));
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
