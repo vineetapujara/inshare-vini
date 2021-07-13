b36c759d1abf886ddac0d2e9e819d8fc23d2658b
vineetapujara <viniaswani70@gmail.com>
2021-07-13 11:06:49 +0530

    commit

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 // Routes
-app.use("api/files", require("./routes/files"));
-app.use("files", require("./routes/show"));
-app.use("files/download", require("./routes/download"));
+app.use("/api/files", require("./routes/files"));
+app.use("/files", require("./routes/show"));
+app.use("/files/download", require("./routes/download"));
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
